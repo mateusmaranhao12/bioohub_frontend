1d0d637d29744abef31a12207403103cf36c5068
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2024-10-12 22:40:31 -0300

    Implement links in usuarios

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -96,13 +96,41 @@
                         </div>
                     </div>
 
-                    <div
-                        class="animate__animated animate__zoomIn card link-card card-maps d-flex flex-column align-items-center justify-content-center">
-                        <i class="fa-solid fa-link fa-2x"></i>
+                    <!--Redes Sociais-->
+                    <div v-for="(rede_social, index) in redes_sociais" :key="index" class="animate__animated animate__zoomIn card link-card card-redes-sociais 
+                        d-flex flex-column align-items-center justify-content-center">
+
+                        <!-- Exibe o nome da rede social -->
+                        <p class="mt-2">{{ getNomeRedeSocial(rede_social.url) }}</p>
+
+                        <!-- Exibe o ícone da rede social -->
+                        <i :class="`fa-brands fa-${detectarRedeSocial(rede_social.url)} fa-2x`"
+                            v-if="detectarRedeSocial(rede_social.url)"></i>
+                        <i class="fa-solid fa-link fa-2x" v-else></i>
+                        <!-- Ícone genérico caso a rede social não seja detectada -->
+
+                        <!-- Botão para abrir o link da rede social -->
+                        <button @click="abrirLink(rede_social.url)" class="btn btn-dark mt-3">
+                            Ir para o link
+                        </button>
                     </div>
-                    <div
-                        class="animate__animated animate__zoomIn card link-card card-links-livres d-flex flex-column align-items-center justify-content-center">
-                        <i class="fa-solid fa-share-alt fa-2x"></i>
+
+                    <!--Links aleatorios-->
+                    <div v-for="(link, index) in links_aleatorios" :key="index" class="animate__animated animate__zoomIn card link-card card-links-livres 
+                        d-flex flex-column align-items-center justify-content-center">
+
+                        <!-- Exibe o nome da rede social -->
+                        <p class="mt-2">{{ getNomeRedeSocial(link.url) }}</p>
+
+                        <!-- Exibe o ícone da rede social -->
+                        <i :class="`fa-brands fa-${detectarRedeSocial(link.url)} fa-2x`"
+                            v-if="detectarRedeSocial(link.url)"></i>
+                        <i class="fa-solid fa-link fa-2x" v-else></i>
+
+                        <!-- Botão para abrir o link da rede social -->
+                        <button @click="abrirLink(link.url)" class="btn btn-dark mt-3">
+                            Ir para o link
+                        </button>
                     </div>
                 </div>
             </div>
@@ -137,6 +165,8 @@ export default class Usuario extends Vue {
     public videos: Array<{ video_url: string }> = []
     public mapas: Array<{ mapa_url: string, nome: string }> = []
     public mapas_footer: Array<{ url: string, titulo: string }> = []
+    public redes_sociais: Array<{ url: string }> = []
+    public links_aleatorios: Array<{ url: string }> = []
 
     public carregando = true // Variável para controlar o estado de carregamento
 
@@ -151,6 +181,12 @@ export default class Usuario extends Vue {
             console.log('Nome de usuário não encontrado ou é inválido.');
             this.usuarioEncontrado = false;
         }
+
+        this.redes_sociais = [
+            { url: 'https://www.instagram.com/user' },
+            { url: 'https://github.com/user' },
+            { url: 'https://www.linkedin.com/in/user' }
+        ];
     }
 
     // Método para obter o ID do vídeo a partir da URL do YouTube
@@ -178,6 +214,8 @@ export default class Usuario extends Vue {
                     const imagens_footer = response.data.imagens_footer || []
                     const mapas = response.data.mapas || []
                     const mapas_footer = response.data.mapas_footer || []
+                    const redes_sociais = response.data.redes_sociais || []
+                    const links_aleatorios = response.data.links_aleatorios || []
 
                     if (perfil) {
                         // Lógica para foto de perfil
@@ -195,8 +233,12 @@ export default class Usuario extends Vue {
                         this.videos = videos;
                     } if (mapas) {
                         this.mapas = mapas
-                    } if(mapas_footer) {
+                    } if (mapas_footer) {
                         this.mapas_footer = mapas_footer
+                    } if (redes_sociais) {
+                        this.redes_sociais = redes_sociais
+                    } if (links_aleatorios) {
+                        this.links_aleatorios = links_aleatorios
                     }
 
                     // Imagens agora são tratadas da mesma forma que a foto de perfil
@@ -240,6 +282,90 @@ export default class Usuario extends Vue {
             console.error('URL do Google Maps inválido:', mapaUrl);
         }
     }
+
+    public abrirLink(url: string) {
+        if (url && url.startsWith('http')) {
+            window.open(url, '_blank'); // Abre o link em uma nova aba
+        } else {
+            console.error('URL inválida:', url);
+        }
+    }
+
+    // Função para detectar a rede social com base na URL e retornar o nome do ícone FontAwesome
+    public detectarRedeSocial(url: string): string | null {
+        if (/https?:\/\/(www\.)?instagram\.com\/[^/]+/.test(url)) {
+            return 'instagram';
+        } else if (/https?:\/\/(www\.)?github\.com\/[^/]+/.test(url)) {
+            return 'github';
+        } else if (/https?:\/\/(www\.)?linkedin\.com\/in\/[^/]+/.test(url)) {
+            return 'linkedin';
+        } else if (/https?:\/\/(www\.)?whatsapp\.com\/[^/]+/.test(url) || /https?:\/\/wa\.me\/[^/]+/.test(url)) {
+            return 'whatsapp';
+        } else if (/https?:\/\/(www\.)?pix\.br\/[^/]+/.test(url)) {
+            return 'pix';
+        } else if (/https?:\/\/(www\.)?twitter\.com\/[^/]+/.test(url)) {
+            return 'twitter';
+        } else if (/https?:\/\/(www\.)?tiktok\.com\/[^/]+/.test(url)) {
+            return 'tiktok';
+        } else if (/https?:\/\/(open\.)?spotify\.com\/[^/]+/.test(url)) {
+            return 'spotify';
+        } else if (/https?:\/\/(www\.)?youtube\.com\/[^/]+/.test(url)) {
+            return 'youtube';
+        } else if (/https?:\/\/(www\.)?vimeo\.com\/[^/]+/.test(url)) {
+            return 'vimeo';
+        } else if (/https?:\/\/(www\.)?substack\.com\/[^/]+/.test(url)) {
+            return 'substack';
+        } else if (/https?:\/\/(www\.)?medium\.com\/[^/]+/.test(url)) {
+            return 'medium';
+        } else if (/https?:\/\/(music\.)?apple\.com\/[^/]+/.test(url)) {
+            return 'apple_music';
+        } else if (/https?:\/\/(www\.)?soundcloud\.com\/[^/]+/.test(url)) {
+            return 'soundcloud';
+        } else if (/https?:\/\/(docs\.)?google\.com\/[^/]+/.test(url)) {
+            return 'google_docs';
+        } else if (/https?:\/\/(drive\.)?google\.com\/[^/]+/.test(url)) {
+            return 'google_drive';
+        } else if (/https?:\/\/(www\.)?notion\.so\/[^/]+/.test(url)) {
+            return 'notion';
+        } else if (/https?:\/\/(www\.)?calendly\.com\/[^/]+/.test(url)) {
+            return 'calendly';
+        } else if (/https?:\/\/(www\.)?producthunt\.com\/[^/]+/.test(url)) {
+            return 'product_hunt';
+        } else if (/https?:\/\/(www\.)?amazon\.com\/[^/]+/.test(url)) {
+            return 'amazon';
+        } else if (/https?:\/\/(calendar\.)?google\.com\/[^/]+/.test(url)) {
+            return 'google_calendar';
+        } else if (/https?:\/\/(www\.)?patreon\.com\/[^/]+/.test(url)) {
+            return 'patreon';
+        } else if (/https?:\/\/(www\.)?twitch\.tv\/[^/]+/.test(url)) {
+            return 'twitch';
+        } else if (/https?:\/\/(www\.)?paypal\.com\/[^/]+/.test(url)) {
+            return 'paypal';
+        } else if (/https?:\/\/(www\.)?kickstarter\.com\/[^/]+/.test(url)) {
+            return 'kickstarter';
+        } else if (/https?:\/\/(www\.)?shopify\.com\/[^/]+/.test(url)) {
+            return 'shopify';
+        } else if (/https?:\/\/(www\.)?gumroad\.com\/[^/]+/.test(url)) {
+            return 'gumroad';
+        } else if (/https?:\/\/(www\.)?eventbrite\.com\/[^/]+/.test(url)) {
+            return 'eventbrite';
+        } else if (/https?:\/\/(www\.)?discord\.com\/[^/]+/.test(url)) {
+            return 'discord';
+        } else if (/https?:\/\/(www\.)?reddit\.com\/r\/[^/]+/.test(url)) {
+            return 'reddit';
+        } else if (/portfolio/.test(url)) {
+            return 'portfolio';
+        } else {
+            return null; // Genérico se não for reconhecida
+        }
+    }
+
+    // Função para retornar o nome da rede social
+    public getNomeRedeSocial(url: string): string {
+        const nome = this.detectarRedeSocial(url);
+        return nome ? nome.charAt(0).toUpperCase() + nome.slice(1) : '';
+    }
+
 }
 </script>
 
